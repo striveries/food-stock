f0c0ed648d7168bd74c62556e522a16b646f6905
striveries <121800213+striveries@users.noreply.github.com>
2023-09-11 13:00:15 +0700

    adding tutorial 1

---
+++ food_stock/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Calista',
+        'class': 'PBP C'
+    }
+
+    return render(request, "main.html", context)
